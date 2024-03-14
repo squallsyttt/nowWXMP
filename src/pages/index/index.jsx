8a294ebd503b4e708909c54c6321523ae0d4b67b
Griffith <390239178@qq.com>
2024-03-14 13:31:50 +0800

    icon 替换

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -6,6 +6,16 @@ import {request} from "../../utils/api";
 import drawIcon from '../../assets/draw.png';
 import listenIcon from '../../assets/listen.png';
 import rightIcon from '../../assets/right.png';
+import starIcon from '../../assets/star.png';
+import unstarIcon from '../../assets/unstar.png';
+import startIcon from '../../assets/start.png';
+import stopIcon from '../../assets/stop.png';
+import timeIcon from '../../assets/time.png';
+import breathIcon44 from '../../assets/44.png';
+import breathIcon478 from '../../assets/478.png';
+import helpIcon from '../../assets/help.png';
+
+
 import VoiceItem from "../../components/voice";
 import SleepItem from "../../components/sleep";
 import voice from "../../components/voice";
@@ -218,7 +228,7 @@ function Index() {
                                 <View className={"text-down"}>-{friendList[0].author}</View>
                             </View>
                             <View className={"content-icon"} onClick={() => jumpContact()}>
-                                <View className={"icon-up"}><img className={"content-icon-img"} src={listenIcon}/></View>
+                                <View className={"icon-up"}><img className={"content-icon-img"} src={helpIcon}/></View>
                                 <View className={"icon-down"}>同行互助</View>
                             </View>
                         </View>
